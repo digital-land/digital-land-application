3ad302c0c5a0e6d8277831857689afa1f06bf218
Adam Shimali <adam.shimali@gmail.com>
2025-01-30 09:34:34 +0000

    add links to spec and diagram

--- application/blueprints/main/views.py
+++ application/blueprints/main/views.py
@@ -1,14 +1,25 @@
+import requests
 from flask import Blueprint, render_template
 
 from application.database.models import Specification
 
 main = Blueprint("main", __name__, template_folder="templates")
 
+SPECIFICATION_URL = "https://digital-land.github.io/specification/specification"
+
 
 @main.route("/")
 def index():
     specification = Specification.query.one_or_none()
+    diagram_url = f"{SPECIFICATION_URL}/{specification.specification}/diagram.svg"
+    try:
+        resp = requests.get(diagram_url)
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError:
+        diagram_url = None
+        print(f"Failed to fetch diagram for {specification.specification}")
     return render_template(
         "index.html",
         specification=specification,
+        diagram_url=diagram_url,
     )
